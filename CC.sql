9eca739e4c13695749bee7baf69a36047d0f5244
Tiago Resende <tiagor87@gmail.com>
2018-03-23 13:58:47 -0300

    Ajuste cohort

--- CC.sql
+++ CC.sql
@@ -1,59 +1,43 @@
--- IF OBJECT_ID('tempdb..#Contratos') IS NOT NULL DROP TABLE #Contratos
--- SELECT
---     C.Id,
---     C.Escritorio,
---     C.Data AS DataEntrada
--- INTO
---     #Contratos
--- FROM
---     Temp_Contracts C
--- WHERE
---     LOWER(C.Status) = 'ativo' and FORMAT(C.Data, 'yyyy/MM') = '2016/03'
-
--- DECLARE @DataEntrada DATETIME, @DataAtual DATETIME;
-
--- SELECT @DataEntrada = MIN(DataEntrada)
--- FROM #Contratos
--- SELECT @DataAtual = GETUTCDATE()
-
--- IF OBJECT_ID('tempdb..#Progresso') IS NOT NULL DROP TABLE #Progresso
--- SELECT
---     FORMAT(DATEFROMPARTS(Year, Month, 1), 'yyyy/MM') AS Cohort,
---     DATEFROMPARTS(Year, Month, 1) AS DataReferencia,
---     Progress
--- INTO
---     #Progresso
--- FROM
---     GetCohortsPeriods(@DataEntrada, @DataAtual)
-
 WITH ContadoresPagantes AS
 (
     SELECT
-        AD.AccountantId AS AccountantId
+        AD.AccountantId,
+        FORMAT(AD.CreateDate, 'yyyy/MM') AS Cohort,
+        AD.CreateDate,
+        AD.Name
     FROM
-        AccountantDetails AD LEFT JOIN AccountantBillingDetails ABD ON AD.AccountantId = ABD.AccountantId
+        AccountantDetails AD (NOLOCK)
+            LEFT JOIN AccountantBillingDetails ABD (NOLOCK) ON AD.AccountantId = ABD.AccountantId
     WHERE
-        AD.AccountantId = '5DF7C9D2-EA19-4C08-A48A-24810829E33E' AND
+--        AD.AccountantId = '3004cba3-474b-4e96-8283-46a54197acaf' AND
         AD.DeleteDate IS NULL AND
-        ABD.BillingStatus = 1 -- PAGANTE
+        ABD.BillingStatus = 1 AND -- PAGANTE
+        FORMAT(AD.CreateDate, 'yyyy/MM') = '2015/11'
 ),
 Organizacoes AS
 (
     SELECT
-    AD.AccountantId,
-    AO.OrganizationId,
-    AO.SubscriptionType,
-    O.TrialEndDate,
-    S.SubscriptionId,
-    O.SubscriptionEndDate,
-    CONVERT(BIT, 
+        AD.AccountantId,
+        AD.CreateDate AS AccountantCreateDate,
+        AO.OrganizationId,
+        AO.SubscriptionType,
+        O.TrialEndDate,
+        S.SubscriptionId,
+        O.CreateDate,
+        O.SubscriptionStartDate,
+        O.SubscriptionEndDate,
+        CONVERT
+        (
+            BIT, 
             CASE
                 WHEN CHARINDEX('importtype=1', AO.Configuration) > 0 THEN 1 
                 WHEN CHARINDEX('importtype=2', AO.Configuration) > 0 THEN 1
                 ELSE 0 
             END
         ) AS ImportEnabled,
-    CONVERT(BIT, 
+        CONVERT
+        (
+            BIT, 
             CASE
                 WHEN CHARINDEX('isinvoicedownload=true', AO.Configuration) > 0 THEN 1 
                 ELSE 0 
@@ -61,29 +45,19 @@ Organizacoes AS
         ) AS IsInvoiceDownload
     FROM
         ContadoresPagantes AD
-        LEFT JOIN AccountantOrganizations AO (NOLOCK) ON AD.AccountantId = AO.AccountantId
-        LEFT JOIN Organizations O (NOLOCK) ON AO.OrganizationId = O.OrganizationId
-        LEFT JOIN Subscriptions S (NOLOCK) ON S.OrganizationId = O.OrganizationId and S.CancelDate is null
+            LEFT JOIN AccountantOrganizations AO (NOLOCK) ON AD.AccountantId = AO.AccountantId
+            LEFT JOIN Organizations O (NOLOCK) ON AO.OrganizationId = O.OrganizationId
+            LEFT JOIN Subscriptions S (NOLOCK) ON S.OrganizationId = O.OrganizationId
     WHERE
+        S.CancelDate IS NULL AND
         AO.DeleteDate IS NULL AND
         O.DeleteDate IS NULL
-),
-TotalEmpresasConectadas AS
-(
-    SELECT
-        O.AccountantId,
-        COUNT(*) AS Count
-    FROM
-        Organizacoes O
-    WHERE
-        O.OrganizationId IS NOT NULL
-    GROUP BY
-        O.AccountantId
 ),
 TotalEmpresasAssinadas AS
 (
     SELECT
-        O.AccountantId,
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM') AS Cohort,
+        FORMAT(O.CreateDate, 'yyyy/MM') AS Month,
         COUNT(*) AS Count
     FROM
         Organizacoes O
@@ -95,16 +69,18 @@ TotalEmpresasAssinadas AS
             O.SubscriptionEndDate >= DATEADD(DAY, 1, DATEADD(MONTH, -3, GETUTCDATE()))
         )
     GROUP BY
-        O.AccountantId
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM'),
+        FORMAT(O.CreateDate, 'yyyy/MM')
 ),
 TotalEmpresasCentralCobranca AS
 (
     SELECT
-        O.AccountantId,
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM') AS Cohort,
+        FORMAT(O.CreateDate, 'yyyy/MM') AS Month,
         COUNT(*) AS Count
     FROM
         Organizacoes O
-            INNER JOIN Configurations C ON O.OrganizationId = C.ReferenceId
+            INNER JOIN Configurations C (NOLOCK) ON O.OrganizationId = C.ReferenceId
     WHERE
         O.SubscriptionType = 1 AND -- Pago pelo contador
         C.[Key] = 'BillingCentralEnabled' AND
@@ -112,52 +88,65 @@ TotalEmpresasCentralCobranca AS
             SELECT
                 1
             FROM
-                [Configurations] Co
+                [Configurations] Co (NOLOCK)
             WHERE
                 Co.ReferenceId = O.OrganizationId AND 
                 Co.[Key] = 'BillingCentralEnabledExpDt' AND
                 Co.Value >= CONVERT(CHAR(10), GETDATE(), 120)
         ) 
     GROUP BY
-        O.AccountantId
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM'),
+        FORMAT(O.CreateDate, 'yyyy/MM')
 ),
 TotalEmpresasGratuitas AS
 (
     SELECT 
-        ACS.AccountantId,
-		SUM(CASE WHEN ACS.Gratuities IS NULL THEN 0 ELSE ACS.Gratuities END) AS Count
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM') AS Cohort,
+        FORMAT(O.CreateDate, 'yyyy/MM') AS Month,
+		SUM
+        (
+            CASE WHEN ACS.Gratuities IS NULL
+                THEN 0
+                ELSE ACS.Gratuities
+            END
+        ) AS Count
 	FROM
-        AccountantSubscriptions ACS (NOLOCK) 
+        AccountantSubscriptions ACS (NOLOCK)
+            INNER JOIN Organizacoes O ON ACS.AccountantId = O.AccountantId
 	WHERE
         ACS.DeletedDate is null
     GROUP BY
-        ACS.AccountantId
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM'),
+        FORMAT(O.CreateDate, 'yyyy/MM')
 ),
 TotalEmpresasCCX AS
 (
     SELECT
-        AO.AccountantId,
+        FORMAT(AO.AccountantCreateDate, 'yyyy/MM') AS Cohort,
+        FORMAT(O.CreateDate, 'yyyy/MM') AS Month,
         COUNT(*) AS Count
     FROM
         Organizacoes AO
-            LEFT JOIN Organizations O ON AO.OrganizationId = O.OrganizationId
-            INNER JOIN Configurations C ON AO.OrganizationId = C.ReferenceId
+            INNER JOIN Organizations O (NOLOCK) ON AO.OrganizationId = O.OrganizationId
+            INNER JOIN Configurations C (NOLOCK) ON AO.OrganizationId = C.ReferenceId
     WHERE
         ISNULL(O.NiboDocsEnabled, 0) = 0 AND
         C.[Key] = 'AccountAndStatementEnabled' AND
         C.Value = 'true'
     GROUP BY
-        AO.AccountantId
+        FORMAT(AO.AccountantCreateDate, 'yyyy/MM'),
+        FORMAT(O.CreateDate, 'yyyy/MM')
 ),
 TotalEmpresasNiboDocs AS
 (
     SELECT
-        AO.AccountantId,
+        FORMAT(AO.AccountantCreateDate, 'yyyy/MM') AS Cohort,
+        FORMAT(O.NiboDocsDateEnabled, 'yyyy/MM') AS Month,
         COUNT(*) AS Count
     FROM
         Organizacoes AO
-            INNER JOIN Organizations O ON AO.OrganizationId = O.OrganizationId
-            INNER JOIN Configurations C ON AO.OrganizationId = C.ReferenceId
+            INNER JOIN Organizations O (NOLOCK) ON AO.OrganizationId = O.OrganizationId
+            INNER JOIN Configurations C (NOLOCK) ON AO.OrganizationId = C.ReferenceId
     WHERE
         O.NiboDocsEnabled = 1 AND
         (
@@ -170,18 +159,20 @@ TotalEmpresasNiboDocs AS
             )
         )
     GROUP BY
-        AO.AccountantId
+        FORMAT(AO.AccountantCreateDate, 'yyyy/MM'),
+        FORMAT(O.NiboDocsDateEnabled, 'yyyy/MM')
 ),
 TotalEmpresasCobrarContador AS
 (
     SELECT
-        O.AccountantId,
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM') AS Cohort,
+        FORMAT(O.CreateDate, 'yyyy/MM') AS Month,
         COUNT(*) - 
-        (SELECT TECCX.Count FROM TotalEmpresasCCX TECCX WHERE TECCX.AccountantId = O.AccountantId) -
-        (SELECT TEND.Count FROM TotalEmpresasNiboDocs TEND WHERE TEND.AccountantId = O.AccountantId) AS Count
+        (SELECT TECCX.Count FROM TotalEmpresasCCX TECCX WHERE TECCX.Cohort = FORMAT(O.AccountantCreateDate, 'yyyy/MM') AND TECCX.Month = FORMAT(O.CreateDate, 'yyyy/MM')) -
+        (SELECT TEND.Count FROM TotalEmpresasNiboDocs TEND WHERE TEND.Cohort = FORMAT(O.AccountantCreateDate, 'yyyy/MM') AND TEND.Month = FORMAT(O.CreateDate, 'yyyy/MM')) AS Count
     FROM
         Organizacoes O
-            LEFT JOIN Configurations C ON O.OrganizationId = C.ReferenceId
+            LEFT JOIN Configurations C (NOLOCK) ON O.OrganizationId = C.ReferenceId
     WHERE
         C.[Key] IN ('AccountAndStatementEnabled', 'IuBillingTypeValue', 'IuBillingType') AND
         O.IsInvoiceDownload = 0 AND
@@ -197,22 +188,34 @@ TotalEmpresasCobrarContador AS
             )
         )
     GROUP BY
-        O.AccountantId
+        FORMAT(O.AccountantCreateDate, 'yyyy/MM'),
+        FORMAT(O.CreateDate, 'yyyy/MM')
+),
+Cohort AS (
+    SELECT
+        FORMAT(RangeStartDate, 'yyyy/MM') AS Cohort,
+        FORMAT(IntervalStartDate, 'yyyy/MM') AS Month,
+        Progress
+    FROM
+        GetCohortsPeriods('2015-11-01', GETUTCDATE())
+        --GetCohortsPeriods((SELECT MIN(CreateDate) FROM ContadoresPagantes), GETUTCDATE())
+    WHERE
+        FORMAT(RangeStartDate, 'yyyy/MM') = '2015/11'
 )
 SELECT
-    CP.AccountantId,
-    ISNULL(TEC.Count, 0) AS 'Total de empresas conectadas',
-    ISNULL(TECC.Count, 0) AS 'Total de empresas para cobrar o contador',
-    ISNULL(TEA.Count, 0) AS 'Total de empresas assinadas',
-    ISNULL(TECeCo.Count, 0) AS 'Total de empresas na central de cobranças',
-    ISNULL(TECCX.Count, 0) AS 'Total de empresas CCX',
-    ISNULL(TEND.Count, 0) AS 'Total de empresas NiboDocs',
+    C.Cohort,
+    C.Month,
+    C.Progress,
+    ISNULL(TECC.Count, 0) AS 'Empresas para cobrar o contador',
+    ISNULL(TEA.Count, 0) AS 'Empresas assinadas',
+    ISNULL(TECeCo.Count, 0) AS 'Empresas na central de cobranças',
+    ISNULL(TECCX.Count, 0) AS 'Empresas CCX',
+    ISNULL(TEND.Count, 0) AS 'Empresas NiboDocs',
     ISNULL(TECC.Count, 0) + ISNULL(TEA.Count, 0) + ISNULL(TECeCo.Count, 0) + ISNULL(TECCX.Count, 0) + ISNULL(TEND.Count, 0) AS 'Total de empresas'
 FROM
-    ContadoresPagantes CP
-        LEFT JOIN TotalEmpresasConectadas TEC ON CP.AccountantId = TEC.AccountantId
-        LEFT JOIN TotalEmpresasCobrarContador TECC ON CP.AccountantId = TECC.AccountantId 
-        LEFT JOIN TotalEmpresasAssinadas TEA ON CP.AccountantId = TEA.AccountantId
-        LEFT JOIN TotalEmpresasCentralCobranca TECeCo ON CP.AccountantId = TECeCo.AccountantId
-        LEFT JOIN TotalEmpresasCCX TECCX ON CP.AccountantId = TECCX.AccountantId
-        LEFT JOIN TotalEmpresasNiboDocs TEND ON CP.AccountantId = TEND.AccountantId
+    Cohort C
+        LEFT JOIN TotalEmpresasCobrarContador TECC ON C.Cohort = TECC.Cohort AND C.Month = TECC.Month
+        LEFT JOIN TotalEmpresasAssinadas TEA ON C.Cohort = TEA.Cohort AND C.Month = TEA.Month
+        LEFT JOIN TotalEmpresasCentralCobranca TECeCo ON C.Cohort = TECeCo.Cohort AND C.Month = TECeCo.Month
+        LEFT JOIN TotalEmpresasCCX TECCX ON C.Cohort = TECCX.Cohort AND C.Month = TECCX.Month
+        LEFT JOIN TotalEmpresasNiboDocs TEND ON C.Cohort = TEND.Cohort AND C.Month = TEND.Month
